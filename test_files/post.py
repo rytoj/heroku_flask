63a6423504b0d5d79d142bfa44ad1d1b171d963c
rytoj <you@example.com>
2017-10-22 23:46:19 +0300

    Add test files

---
+++ test_files/post.py
@@ -0,0 +1,14 @@
+import base64
+
+import os
+import requests
+
+config_file = "testfile.conf"
+with open(os.path.join(config_file), "rb") as file:
+    file_read = file.read()
+
+json_data = {config_file: file_read.decode(),
+             "password": "c3VwCg=="}
+headers = {"content-type": "application/json",
+           "pass": "secret"}
+requests.post(url="http://127.0.0.1:5000/upload", headers=headers, json=json_data)

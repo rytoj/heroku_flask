a81cc1027216dc084f0837f04e35aaa57b1a5d42
Paulius Gaižauskas <paulius.gaizauskas@visma.com>
2017-10-23 10:17:08 +0300

    Refactor code

--- test_files/post.py
+++ test_files/post.py
@@ -1,5 +1,8 @@
 import os
+
 import requests
+
+URL = "http://127.0.0.1:5000/upload"
 
 config_file = "testfile.conf"
 with open(os.path.join(config_file), "rb") as file:
@@ -9,4 +12,4 @@ json_data = {config_file: file_read.decode(),
              "password": "c3VwCg=="}
 headers = {"content-type": "application/json",
            "pass": "secret"}
-requests.post(url="http://127.0.0.1:5000/upload", headers=headers, json=json_data)
+requests.post(url=URL, headers=headers, json=json_data)
